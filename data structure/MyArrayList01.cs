2af7bf25c942d1fc71b80ddb6bba134bf529538c
Yuri <wenlongjing00@gmail.com>
2023-02-22 20:08:13 +0900

    init2

---
+++ data structure/MyArrayList01.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace data_structure
+{
+    internal class MyArrayList01
+    {
+        private int [] Data;//定义一个数组
+        private int N;      //定义一个变量记录数组的容量
+        /// <summary>
+        ///  
+        /// </summary>
+        /// <param name="Capacity">默认集合长度</param>
+        public MyArrayList01(int Capacity)
+        {
+            Data = new int [ Capacity ];//初始化数组长度
+            N = 0;                      //初始化数组容量
+        }
+    }
+}
